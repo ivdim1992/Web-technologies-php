30ad3551bead929ffe79ec45d3445d15594fa307
ivdim <ivdim1992@gmail.com>
2023-11-20 08:41:37 +0200

    update and delete post added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,9 +25,15 @@
     <div style="border: 3px solid black">
       <h2>All posts</h2>
       @foreach($posts as $post)
-    <div style="background-color:gray; padding:10px; margin:10px;">
-<h3>{{$post['title']}}</div>
-<div>{{$post['body']}}</div>
+    <div style="background-color:gray; padding:10px; margin:10px">
+        <h3>{{$post['title']}} by {{$post->user->name}}</h3>
+        <div>{{$post['body']}}</div>
+        <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
+        <form action="/delete-post/{{$post->id}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button>DELETE</button>
+        </form>
       @endforeach
     </div>
 
